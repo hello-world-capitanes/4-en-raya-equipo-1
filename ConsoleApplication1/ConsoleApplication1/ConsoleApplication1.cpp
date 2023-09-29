f4b46f93c8f80a1f292519bfc4edccd41caccba3
BABEL-SI\sergio.claver <sergio.fclaver@alumnos.upm.es>
2023-09-29 09:37:45 +0200

    3 en raya ia

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,85 +1,13 @@
 #include <iostream>
 #include <string.h>
+#include "TresEnRaya.h"
+
 using namespace std;
 //----------------------------------------------------------------------------------------------------------------------------------
 const int filas = 6;
 const int columnas = 7;
 
 char tablero[filas][columnas];
-
-void inicializarTablero() {
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
-            tablero[i][j] = ' ';
-        }
-    }
-}
-
-void mostrarTablero() {
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
-            cout << "| " << tablero[i][j] << " ";
-        }
-        cout << "|" << endl;
-    }
-}
-
-bool validarMovimiento(int fila, int col) {
-    if (fila < 0 || fila >= filas || col < 0 || col >= columnas) {
-        cout << "Posición no válida. 0-5 fila, 0-6 columna" << endl;
-        return false;
-    }
-    if (tablero[fila][col] != ' ') {
-        cout << "Casilla ocupada." << endl;
-        return false;
-    }
-    return true;
-}
-
-bool verificarGanador(char jugador) {
-    // Horizontal
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j <= columnas - 4; j++) { //El -4 es para que no intente a acceder a elementos que no existen y se valide el minion de 4 en raya
-            if (tablero[i][j] == jugador && tablero[i][j + 1] == jugador && tablero[i][j + 2] == jugador && tablero[i][j + 3] == jugador)
-                return true;
-        }
-    }
-
-    // Vertical
-    for (int i = 0; i <= filas - 4; i++) { //Igual que antes pero con filas
-        for (int j = 0; j < columnas; j++) {
-            if (tablero[i][j] == jugador && tablero[i + 1][j] == jugador && tablero[i + 2][j] == jugador && tablero[i + 3][j] == jugador)
-                return true;
-        }
-    }
-
-    // Diagonal izq der
-    for (int i = 0; i <= filas - 4; i++) { //Lo mismo pero doble xq es diagonal
-        for (int j = 0; j <= columnas - 4; j++) {
-            if (tablero[i][j] == jugador && tablero[i + 1][j + 1] == jugador && tablero[i + 2][j + 2] == jugador && tablero[i + 3][j + 3] == jugador)
-                return true;
-        }
-    }
-
-    // Diagonal der izq
-    for (int i = 0; i <= filas - 4; i++) {
-        for (int j = 3; j < columnas; j++) { //Aqui no se hace xq empiexo en 3 y se desplaza en --
-            if (tablero[i][j] == jugador && tablero[i + 1][j - 1] == jugador && tablero[i + 2][j - 2] == jugador && tablero[i + 3][j - 3] == jugador)
-                return true;
-        }
-    }
-    return false;
-}
-
-bool tableroLleno() {
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
-            if (tablero[i][j] == ' ')
-                return false;
-        }
-    }
-    return true;
-}
 
 int main() {
     char jugadorActual = 'X';
---
+++ ConsoleApplication1/ConsoleApplication1/TresEnRaya.h
@@ -0,0 +1,10 @@
+#ifndef TresEnRaya.h
+#define TresEnRaya.h
+
+void inicializarTablero();
+void mostrarTablero();
+bool validarMovimiento(int fila, int col);
+bool verificarGanador(char jugador);
+bool tableroLleno();
+
+#endif
